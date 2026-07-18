ac54acc9b29a340f388bacca372f03de0abe69db
anu-git-lab <rejinoldanu23@gmail.com>
2026-07-18 09:39:29 +0530

    refactor: update number guessing game logic and enhance UI styling

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-let num= Math.random() * 10+1;
+// 
 
-let roundedNum = Math.floor(num);
+let correctAnswer = Math.floor(Math.random() * 10) + 1;
+let attempts = 3;
+let count = 0;
 
-console.log(roundedNum);
+function addGuess() {
+
+    let guess = document.getElementById("guessInput").value;
+    let message = document.getElementById("message");
+
+    count++;
+
+    if (guess == correctAnswer) {
+        message.innerHTML = "Correct! You guessed the number.";
+    } 
+    else if (count >= attempts) {
+        message.innerHTML = 
+        "Your number of attempts has exceeded. The correct answer is " 
+        + correctAnswer + ".";
+    } 
+    else {
+        message.innerHTML = "Wrong guess. Try again.";
+    }
+
+    document.getElementById("guessInput").value = "";
+}
