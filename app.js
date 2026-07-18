27eaae568d9a4c543d69fa4034e5db9dcf43f66c
anu-git-lab <rejinoldanu23@gmail.com>
2026-07-18 10:23:27 +0530

    feat: add SweetAlert2 for feedback on correct and incorrect guesses

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ function addGuess() {
 
     if (guess == correctAnswer) {
         message.innerHTML = "Correct! You guessed the number.";
+        Swal.fire({
+  title: "Sweet!",
+  text: "Modal with a custom image.",
+  imageUrl: "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExN3IybWdycTIxeWl6bHA5OTJsMGNrdWpnN2Q4MGIzYzJzaHlwYzVpYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vu5UbNpjpqfMq2UFg0/giphy.gif",
+  imageWidth: 400,
+  imageHeight: 200,
+//    imageAlt: "Custom image"
+});
     } 
     else if (count >= attempts) {
         message.innerHTML = 
@@ -21,6 +29,12 @@ function addGuess() {
     } 
     else {
         message.innerHTML = "Wrong guess. Try again.";
+        Swal.fire({
+  icon: "error",
+  title: "Oops...",
+  text: "Wrong guess!",
+//   footer: "<a href=\"#\">Why do I have this issue?</a>"
+});
     }
 
     document.getElementById("guessInput").value = "";
